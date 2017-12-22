7ddb6aa861ee0ef1cbc24baa85f2c8d7c780bb83
Simon Lacroix <simon@miaou.be>
2017-12-22 21:07:37 +0100

    Enable automated tests on docker cloud (#1)

---
+++ test/mocys_booking_test.js
@@ -0,0 +1,10 @@
+require('should');
+// const MacysBooking = require('./macys_booking');
+
+describe('My App', () => {
+  it('should test something', (done) => {
+    const x = 1;
+    x.should.eql(1);
+    done();
+  });
+});
