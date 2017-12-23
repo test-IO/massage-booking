b4f18291d9645ec69d0133bd84e76047b501d30d
Simon Lacroix <simon@miaou.be>
2017-12-23 18:18:39 +0100

    Some tests

--- macys_booking.js
+++ macys_booking.js
@@ -11,47 +11,9 @@ function sendMessageToSlackResponseUrl(responseUrl, jsonMessage, callback) {
   request(postOptions, callback);
 }
 
-
 class MacysBooking {
   constructor(slackWebClient) {
     this.slackWebClient = slackWebClient;
-  }
-
-  bookMassage(payload, callback) {
-    const attachments = [
-      {
-        text: 'There is one spot available at HH:MM, do you want to reserve it?',
-        fallback: "Shame... buttons aren't supported in this land",
-        callback_id: 'book-massage',
-        color: '#3AA3E3',
-        attachment_type: 'default',
-        actions: [
-          {
-            name: 'reserve',
-            text: 'Yes, reserve',
-            type: 'button',
-            value: 'HH:MM',
-          },
-          {
-            name: 'cancel',
-            text: 'Cancel',
-            type: 'button',
-            value: 'cancel',
-          },
-        ],
-      },
-    ];
-
-    sendMessageToSlackResponseUrl(payload.response_url, { attachments }, callback);
-
-
-    // web.chat.postMessage(payload.channel_id, 'Test', { attachments }, (err, res) => {
-    //   if (err) {
-    //     console.log('Error:', err);
-    //   } else {
-    //     console.log('Message sent: ', res);
-    //   }
-    // });
   }
 
   actionHandler(payload, callback) {
@@ -74,6 +36,51 @@ class MacysBooking {
     //     console.log('Message sent: ', res);
     //   }
     // });
+  }
+
+  bookMassage(payload, callback) {
+    const nextAvailability = this.nextAvailability();
+    const attachments = [
+      {
+        text: `There is one spot available at ${nextAvailability}, do you want to reserve it?`,
+        callback_id: 'book-massage',
+        attachment_type: 'default',
+        actions: [
+          {
+            name: 'reserve',
+            text: `Yes, reserve ${nextAvailability}`,
+            type: 'button',
+            value: nextAvailability,
+          },
+          {
+            name: 'reserve',
+            text: 'Pick a another time...',
+            type: 'select',
+            options: [
+              { text: '14:30', value: '14:30' },
+              { text: '15:45', value: '15:45' },
+            ],
+          },
+        ],
+      },
+    ];
+
+    sendMessageToSlackResponseUrl(payload.response_url, { attachments }, callback);
+
+
+    // web.chat.postMessage(payload.channel_id, 'Test', { attachments }, (err, res) => {
+    //   if (err) {
+    //     console.log('Error:', err);
+    //   } else {
+    //     console.log('Message sent: ', res);
+    //   }
+    // });
+  }
+
+  nextAvailability() {
+    const currentTimeInMs = new Date(Date.now());
+    const currentTime = new Date(currentTimeInMs);
+    return `${currentTime.getHours()}:${currentTime.getMinutes()}`;
   }
 }
 
--- test/mocys_booking_test.js
+++ test/mocys_booking_test.js
@@ -13,8 +13,66 @@ describe('MacysBooking', () => {
     macysBooking = new MacysBooking(slackWebClient);
   });
 
+  describe('#actionHandler', () => {
+    context('book-massage', () => {
+      context('reserve', () => {
+        it('clicked on reserve button', (done) => {
+          const payload = {
+            type: 'interactive_message',
+            actions: [{ name: 'reserve', type: 'button', value: '17:33' }],
+            callback_id: 'book-massage',
+            team: { id: 'T25MRFT3M' },
+            channel: { id: 'C8HTS5MEC' },
+            user: { id: 'U25PP0KEE', name: 'simon' },
+            action_ts: '1514046806.960158',
+            message_ts: '1514046805.000007',
+            attachment_id: '1',
+            is_app_unfurl: false,
+            response_url: 'https://hooks.slack.com/actions/T25MRFT3M/290209823664/Dtfv5c9DWE7nh0wqVOYB2n8t',
+            trigger_id: '291932495047.73739537123.5a0db0f14d1768425d6c498ffe0eac72',
+          };
+
+          const slackCall = nock('https://hooks.slack.com:443', { encodedQueryParams: true })
+            .post('/actions/T25MRFT3M/290209823664/Dtfv5c9DWE7nh0wqVOYB2n8t', { attachments: [{ text: 'simon clicked: reserve' }], replace_original: true })
+            .reply(200, 'ok');
+
+          macysBooking.actionHandler(payload, () => {
+            slackCall.done();
+            done();
+          });
+        });
+
+        it('selected a time', (done) => {
+          const payload = {
+            type: 'interactive_message',
+            actions: [{ name: 'reserve', type: 'select', selected_options: [] }],
+            callback_id: 'book-massage',
+            team: { id: 'T25MRFT3M' },
+            channel: { id: 'C8HTS5MEC' },
+            user: { id: 'U25PP0KEE', name: 'simon' },
+            action_ts: '1514046813.882252',
+            message_ts: '1514046811.000025',
+            attachment_id: '1',
+            is_app_unfurl: false,
+            response_url: 'https://hooks.slack.com/actions/T25MRFT3M/290870923474/D5EQmPpOCph5nhjxCVUnXC6n',
+            trigger_id: '290793622739.73739537123.14c350eed428152158ecf12061041e20',
+          };
+
+          const slackCall = nock('https://hooks.slack.com:443', { encodedQueryParams: true })
+            .post('/actions/T25MRFT3M/290870923474/D5EQmPpOCph5nhjxCVUnXC6n', { attachments: [{ text: 'simon clicked: reserve' }], replace_original: true })
+            .reply(200, 'ok');
+
+          macysBooking.actionHandler(payload, () => {
+            slackCall.done();
+            done();
+          });
+        });
+      });
+    });
+  });
+
   describe('#bookMassage', () => {
-    it('should send a form to slack', (done) => {
+    it('send a form to reserve the current time (for now)', (done) => {
       const payload = {
         token: '',
         team_id: 'T25MRFT3M',
@@ -26,21 +84,33 @@ describe('MacysBooking', () => {
         trigger_id: '290064239264.73739537123.0cb6e21b315eff944b90b083405e102c',
       };
 
+      const nextAvailability = macysBooking.nextAvailability();
+      const attachments = [
+        {
+          text: `There is one spot available at ${nextAvailability}, do you want to reserve it?`,
+          callback_id: 'book-massage',
+          attachment_type: 'default',
+          actions: [
+            {
+              name: 'reserve',
+              text: `Yes, reserve ${nextAvailability}`,
+              type: 'button',
+              value: nextAvailability,
+            },
+            {
+              name: 'reserve',
+              text: 'Pick a another time...',
+              type: 'select',
+              options: [
+                { text: '14:30', value: '14:30' },
+                { text: '15:45', value: '15:45' },
+              ],
+            },
+          ],
+        },
+      ];
       const slackCall = nock('https://hooks.slack.com:443', { encodedQueryParams: true })
-        .post('/commands/T25MRFT3M/290865925813/ZJM12v4tsId9wbDyjDoYa5Hb', {
-          attachments: [{
-            text: 'There is one spot available at HH:MM, do you want to reserve it?',
-            fallback: "Shame... buttons aren't supported in this land",
-            callback_id: 'book-massage',
-            color: '#3AA3E3',
-            attachment_type: 'default',
-            actions: [{
-              name: 'reserve', text: 'Yes, reserve', type: 'button', value: 'HH:MM',
-            }, {
-              name: 'cancel', text: 'Cancel', type: 'button', value: 'cancel',
-            }],
-          }],
-        })
+        .post('/commands/T25MRFT3M/290865925813/ZJM12v4tsId9wbDyjDoYa5Hb', { attachments })
         .reply(200, 'ok');
 
       macysBooking.bookMassage(payload, () => {
