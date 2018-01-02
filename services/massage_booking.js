80d55d328fa53c7fc2567828d1cd19dbaff36da4
Simon Lacroix <simon@miaou.be>
2018-01-02 03:27:26 +0100

    massage-booking is using booking-repository

--- services/massage_booking.js
+++ services/massage_booking.js
@@ -34,7 +34,6 @@ function timeToString(date) {
 
 class MassageBooking {
   constructor(slackWebClient, bookingDuration = 20) {
-    this.reservations = [];
     this.bookingDuration = bookingDuration;
     this.bookingRepository = new BookingRepository();
     this.slackWebClient = slackWebClient;
@@ -44,118 +43,131 @@ class MassageBooking {
     const selectedDate = timeFromString(payload.actions[0].type === 'select' ? payload.actions[0].selected_options[0].value : payload.actions[0].value);
     const dateRange = new DateRange(selectedDate, addMinutes(selectedDate, this.bookingDuration));
 
-    if (this.dateRangeAvailable(payload.user.id, dateRange)) {
-      this.findUserById(payload.user.id, (error, user) => {
-        if (error) {
-          callback(error);
-          return;
-        }
+    this.bookingRepository.all().catch(callback).then((bookings) => {
+      const bookingaaaa = bookings.find(b => b.user.id !== payload.user.id && b.dateRange.isIntersecting(dateRange));
+      if (typeof bookingaaaa === 'undefined') {
+        this.findUserById(payload.user.id, (error, user) => {
+          if (error) {
+            callback(error);
+            return;
+          }
 
-        const previousReservation = this.findBookingForUserId(user.id);
+          const now = new Date();
+          const previousReservation = bookings.filter(reservation => reservation.dateRange.end > now)
+            .find(reservation => reservation.user.id === user.id);
 
-        this.removeBookingsForUserId(user.id);
-        this.addBooking(new Booking(user, dateRange));
+          bookings.forEach((booking) => {
+            if (booking.user.id === user.id) {
+              this.bookingRepository.remove(booking).catch(callback);
+            }
+          });
 
+          this.bookingRepository.add(new Booking(user, dateRange)).catch(callback).then(() => {
+            const message = {
+              attachments: [],
+              replace_original: true,
+            };
+            if (typeof previousReservation === 'undefined') {
+              message.attachments.push({
+                text: `Thanks for your booking at ${timeToString(dateRange.start)} -> ${timeToString(dateRange.end)}`,
+              });
+            } else {
+              message.attachments.push({
+                text: `Your booking as been successfully updated to ${timeToString(dateRange.start)} -> ${timeToString(dateRange.end)}`,
+              });
+            }
+
+            sendMessageToSlackResponseUrl(payload.response_url, message, callback);
+          });
+        });
+      } else {
         const message = {
-          attachments: [],
+          attachments: [
+            {
+              text: 'Sorry but this time is not available anymore.',
+            },
+          ],
           replace_original: true,
         };
-        if (typeof previousReservation === 'undefined') {
-          message.attachments.push({
-            text: `Thanks for your booking at ${timeToString(dateRange.start)} -> ${timeToString(dateRange.end)}`,
-          });
-        } else {
-          message.attachments.push({
-            text: `Your booking as been successfully updated to ${timeToString(dateRange.start)} -> ${timeToString(dateRange.end)}`,
-          });
-        }
 
         sendMessageToSlackResponseUrl(payload.response_url, message, callback);
-      });
-    } else {
-      const message = {
-        attachments: [
-          {
-            text: 'Sorry but this time is not available anymore.',
-          },
-        ],
-        replace_original: true,
-      };
-
-      sendMessageToSlackResponseUrl(payload.response_url, message, callback);
-    }
-  }
-
-  addBooking(reservation) {
-    this.reservations.push(reservation);
+      }
+    });
   }
 
   bookMassage(payload, callback) {
     if (payload.text === 'list') {
       const now = new Date();
-      const attachments = this.reservations.filter(reservation => reservation.dateRange.end > now).sort((a, b) => a.dateRange.start - b.dateRange.start).map((reservation) => {
-        const attachment = {
-          text: `${timeToString(reservation.dateRange.start)} -> ${timeToString(reservation.dateRange.end)}   ${reservation.user.realName}`,
-        };
-        if (reservation.user.id === payload.user_id) {
-          attachment.color = '#36a64f';
-        }
-        return attachment;
-      });
-
-      if (attachments.length === 0) {
-        attachments.push({ text: 'There is no booking yet, book a massage!', color: '#36a64f' });
-      }
-
-      sendMessageToSlackResponseUrl(payload.response_url, { attachments }, callback);
-    } else {
-      const nextAvailabilities = this.findAvailabilities(payload.user_id, 25);
-      const nextAvailabilityString = timeToString(nextAvailabilities[0]);
-      const previousReservation = this.findBookingForUserId(payload.user_id);
-
-      const attachments = [
-        {
-          text: `There is one spot available at ${nextAvailabilityString}, do you want to reserve it?`,
-          color: '#36a64f',
-          callback_id: 'book-massage',
-          attachment_type: 'default',
-          actions: [
-            {
-              name: 'reserve',
-              text: `Yes, reserve ${timeToString(nextAvailabilities[0])} -> ${timeToString(addMinutes(nextAvailabilities[0], this.bookingDuration))}`,
-              type: 'button',
-              value: nextAvailabilityString,
-            },
-            {
-              name: 'reserve',
-              text: 'Pick a another time...',
-              type: 'select',
-              options: nextAvailabilities.map(date => ({
-                text: `${timeToString(date)} -> ${timeToString(addMinutes(date, this.bookingDuration))}`,
-                value: timeToString(date),
-              })),
-            },
-          ],
-        },
-      ];
-
-      if (typeof previousReservation !== 'undefined') {
-        attachments.push({
-          text: `Note: You already have a booking for ${timeToString(previousReservation.dateRange.start)} -> ${timeToString(previousReservation.dateRange.end)}.\nMaking a new booking will cancel the previous ones.`,
-          color: '#ffcc00',
+      this.bookingRepository.all().catch(callback).then((bookings) => {
+        const attachments = bookings.filter(reservation => reservation.dateRange.end > now).sort((a, b) => a.dateRange.start - b.dateRange.start).map((reservation) => {
+          const attachment = {
+            text: `${timeToString(reservation.dateRange.start)} -> ${timeToString(reservation.dateRange.end)}   ${reservation.user.realName}`,
+          };
+          if (reservation.user.id === payload.user_id) {
+            attachment.color = '#36a64f';
+          }
+          return attachment;
         });
-      }
 
-      sendMessageToSlackResponseUrl(payload.response_url, { attachments }, callback);
+        if (attachments.length === 0) {
+          attachments.push({ text: 'There is no booking yet, book a massage!', color: '#36a64f' });
+        }
+
+        sendMessageToSlackResponseUrl(payload.response_url, { attachments }, callback);
+      });
+    } else {
+      this.findAvailabilities(payload.user_id, 25, (error, nextAvailabilities) => {
+        if (error) {
+          callback(error);
+          return;
+        }
+
+        this.bookingRepository.all().catch(callback).then((bookings) => {
+          const nextAvailabilityString = timeToString(nextAvailabilities[0]);
+          const now = new Date();
+          const previousReservation = bookings.filter(booking => booking.dateRange.end > now).find(booking => booking.user.id === payload.user_id);
+
+          const attachments = [
+            {
+              text: `There is one spot available at ${nextAvailabilityString}, do you want to book it?`,
+              color: '#36a64f',
+              callback_id: 'book-massage',
+              attachment_type: 'default',
+              actions: [
+                {
+                  name: 'book',
+                  text: `Yes, book ${timeToString(nextAvailabilities[0])} -> ${timeToString(addMinutes(nextAvailabilities[0], this.bookingDuration))}`,
+                  type: 'button',
+                  value: nextAvailabilityString,
+                },
+                {
+                  name: 'book',
+                  text: 'Pick a another time...',
+                  type: 'select',
+                  options: nextAvailabilities.map(date => ({
+                    text: `${timeToString(date)} -> ${timeToString(addMinutes(date, this.bookingDuration))}`,
+                    value: timeToString(date),
+                  })),
+                },
+              ],
+            },
+          ];
+
+          if (typeof previousReservation !== 'undefined') {
+            attachments.push({
+              text: `Note: You already have a booking for ${timeToString(previousReservation.dateRange.start)} -> ${timeToString(previousReservation.dateRange.end)}.\nMaking a new booking will cancel the previous ones.`,
+              color: '#ffcc00',
+            });
+          }
+
+          sendMessageToSlackResponseUrl(payload.response_url, { attachments }, callback);
+        });
+      });
     }
   }
 
-  dateRangeAvailable(userId, dateRange) {
-    const intersectedReservation = this.reservations.find(reservation => reservation.user.id !== userId && reservation.dateRange.isIntersecting(dateRange));
-    return typeof intersectedReservation === 'undefined';
-  }
-
-  findAvailabilities(userId, maxAvalaibilities = 10, minutesPerStep = 5) {
+  findAvailabilities(userId, maxAvalaibilities = 10, callback) {
+    const minutesPerStep = 5;
     const availabilities = [];
     let iterator = new Date();
     const maxAvalaibilityDate = new Date(iterator.getFullYear(), iterator.getMonth(), iterator.getDate(), 23, 59, 999);
@@ -174,21 +186,17 @@ class MassageBooking {
     iterator.setSeconds(0);
     iterator.setMilliseconds(0);
 
-    while (availabilities.length < maxAvalaibilities && iterator < maxAvalaibilityDate) {
-      const dateRange = new DateRange(iterator, addMinutes(iterator, this.bookingDuration));
-      if (this.dateRangeAvailable(userId, dateRange)) {
-        availabilities.push(new Date(iterator));
+    this.bookingRepository.all().catch(callback).then((bookings) => {
+      while (availabilities.length < maxAvalaibilities && iterator < maxAvalaibilityDate) {
+        const dateRange = new DateRange(iterator, addMinutes(iterator, this.bookingDuration));
+        const booking = bookings.find(b => b.user.id !== userId && b.dateRange.isIntersecting(dateRange));
+        if (typeof booking === 'undefined') {
+          availabilities.push(new Date(iterator));
+        }
+        iterator = addMinutes(iterator, minutesPerStep);
       }
-      iterator = addMinutes(iterator, minutesPerStep);
-    }
-
-    return availabilities;
-  }
-
-  findBookingForUserId(userId) {
-    const now = new Date();
-    return this.reservations.filter(reservation => reservation.dateRange.end > now)
-      .find(reservation => reservation.user.id === userId);
+      callback(null, availabilities);
+    });
   }
 
   findUserById(userId, callback) {
@@ -199,11 +207,6 @@ class MassageBooking {
         callback(undefined, new User(result.user.id, result.user.name, result.user.real_name));
       }
     });
-  }
-
-  removeBookingsForUserId(userId) {
-    const now = new Date();
-    this.reservations = this.reservations.filter(reservation => reservation.dateRange.end > now && reservation.user.id !== userId);
   }
 }
 
