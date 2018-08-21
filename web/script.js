1cc72aac8d1caddde5567c106874c406830c7a80
Simon Lacroix <simon@miaou.be>
2018-08-21 23:10:17 +0200

    Auto fix some lint issues

--- web/script.js
+++ web/script.js
@@ -3,51 +3,52 @@ rivets.configure({
   preloadData: true,
   rootInterface: '.',
   templateDelimiters: ['{', '}'],
-  iterationAlias : function(modelName) {
-    return '%' + modelName + '%';
+  iterationAlias(modelName) {
+    return `%${modelName}%`;
   },
-  handler: function(target, event, binding) {
-    this.call(target, event, binding.view.models)
+  handler(target, event, binding) {
+    this.call(target, event, binding.view.models);
   },
-  executeFunctions: false
-})
+  executeFunctions: false,
+});
 
-rivets.formatters.eq = function(val, arg){
-  return val == arg
-}
+rivets.formatters.eq = function (val, arg) {
+  return val == arg;
+};
 
-rivets.formatters.date = function(value){
+rivets.formatters.date = function (value) {
   return moment(value).fromNow();
-}
+};
 
-rivets.formatters.time = function(value){
+rivets.formatters.time = function (value) {
   return moment(value).format('HH:mm');
-}
+};
 
-var state = {
+const state = {
   running: false,
   empty: true,
   current_booking: { user: { realName: 'loading...' } },
-  upcoming_bookings: []
-}
+  upcoming_bookings: [],
+};
 
-var update = function() {
-  $.get('/bookings', function(data, status){
-    window.state.empty = data.bookings.length == 0;
+const update = function () {
+  $.get('/bookings', (data, status) => {
+    window.state.empty = data.bookings.length === 0;
     window.state.current_booking = data.bookings.shift();
     window.state.upcoming_bookings = data.bookings;
-    if (typeof window.state.current_booking != "undefined") {
-      var start = moment(window.state.current_booking.dateRange.start);
-      var end = moment(window.state.current_booking.dateRange.end);
+    if (typeof window.state.current_booking !== 'undefined') {
+      const start = moment(window.state.current_booking.dateRange.start);
+      const end = moment(window.state.current_booking.dateRange.end);
       window.state.running = (start < moment() && end > moment());
     } else {
       window.state.running = false;
     }
   });
-}
+};
 
-$(function() {
+$(() => {
   rivets.bind($('body'), state);
+  window.state = state;
   update();
   setInterval(update, 10000);
 });
